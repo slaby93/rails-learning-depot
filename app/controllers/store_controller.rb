092e566bded62899e0f49eba24200a4116ba2409
Daniel Slaby <pufek93@gmail.com>
2018-08-29 09:47:58 +0200

    add ability to add line_items to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  before_action :initialize_counter, only: :index
+
   def index
+    session[:counter] += 1
     @products = Product.order(:title)
   end
+
+  def initialize_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+  end
+
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -18,7 +18,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
+      post products_url, params: {product: {description: @product.description, image_url: @product.image_url, price: @product.price, title: @title}}
     end
 
     assert_redirected_to product_url(Product.last)
@@ -35,7 +35,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
+    patch product_url(@product), params: {product: {description: @product.description, image_url: @product.image_url, price: @product.price, title: @title}}
     assert_redirected_to product_url(@product)
   end
 
@@ -45,5 +45,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to products_url
+  end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
   end
 end
